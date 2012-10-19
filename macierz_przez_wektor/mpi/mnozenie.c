47ea47ecbaad0d07e2baba1a4e40ede77ae70aad
Jan Mudry <jairek@o2.pl>
2012-10-20 00:48:09 +0200

    makefile dodany

--- macierz_przez_wektor/mpi/mnozenie.c
+++ macierz_przez_wektor/mpi/mnozenie.c
@@ -21,83 +21,85 @@ int main(int argc, char **argv){
 		MPI_Comm_size(MPI_COMM_WORLD, &np);	
 		
 		if (sscanf(argv[1], "%d", &size)) {
-			long int start;
-			double sum_execution_time = 0;
-			int count_loop_main = 100;
-			double execution_time;
+			if (size % np == 0) {
+				long int start;
+				double sum_execution_time = 0;
+				int count_loop_main = 100;
+				double execution_time;
 			
-			int podzial = (int)size/np;
-			int matrix[size][size]; // [kolumna, wiersz]
-			int vector[size];
-			int result[size];
-			int res_result[podzial];
-			int i,j,k,l;
-			int res_matrix[size][podzial]; // [kolumna, wiersz]
+				int podzial = (int)size/np;
+				int matrix[size][size]; // [kolumna, wiersz]
+				int vector[size];
+				int result[size];
+				int res_result[podzial];
+				int i,j,k,l;
+				int res_matrix[size][podzial]; // [kolumna, wiersz]
 		
-			for (l = 0; l < count_loop_main; l++) {
+				for (l = 0; l < count_loop_main; l++) {
 				
-				srand(time(0));
+					srand(time(0));
 
-				// inicjalizacja wektora rozwiązania
-				for (i =0; i < podzial; i++) {
-					res_result[i] = 0;
-				}	
+					// inicjalizacja wektora rozwiązania
+					for (i =0; i < podzial; i++) {
+						res_result[i] = 0;
+					}	
 			
-				// randomowe wartości
-				if (rank == ROOT) {
-					for (i = 0; i < size; i++){
-						for (j = 0; j < size; j++){
-							matrix[i][j] = (int)(rand() % 10);	
-						}
-						vector[i] =  (int)(rand() % 10);	
-					}
-				}
-
-				start = czas();
-				/* Wysłanie czśsci macierzy do procesów */
-				for (k =0; k< size; k++ ){
-					MPI_Scatter(matrix[k], podzial, MPI_INT, &res_matrix[k], podzial, MPI_INT, ROOT, MPI_COMM_WORLD); 
-				}
-				/* Wysłanie vektora do wszystkich */
-				MPI_Bcast( vector, size, MPI_INT, 0,  MPI_COMM_WORLD);	
-		
-				//właściwe swojej części przez wektor
-				for (i = 0; i < size; i++){
-					for (j = 0; j < size; j++){
-						res_result[i] += res_matrix[j][i] * vector[j];
-					}
-				}
-				
-				MPI_Gather(&res_result, podzial, MPI_INT, result, podzial, MPI_INT, ROOT, MPI_COMM_WORLD);	
-				execution_time = (double)(czas() - start)/(double)TIMER_SCALE; 
-				sum_execution_time += execution_time;
-				//wyświetlanie wyniku
-				if (rank == ROOT) {
-					if (size <= 16) {
+					// randomowe wartości
+					if (rank == ROOT) {
 						for (i = 0; i < size; i++){
 							for (j = 0; j < size; j++){
-								printf("%d\t",matrix[j][i]);
+								matrix[i][j] = (int)(rand() % 10);	
 							}
-							printf("| %d\t|", vector[i]);
-							printf("| %d\t|\n", result[i]);
+							vector[i] =  (int)(rand() % 10);	
 						}
 					}
-				//	printf("\nKoniec obliczeń, czas obliczeń = %f\n", execution_time);
-				}
-				
-			} 
-			if (rank == ROOT) {
-				printf("\nKoniec obliczeń, średni czas obliczeń = %f\n", (double)execution_time/count_loop_main);
-			}
 
+					start = czas();
+					/* Wysłanie czśsci macierzy do procesów */
+					for (k =0; k< size; k++ ){
+						MPI_Scatter(matrix[k], podzial, MPI_INT, &res_matrix[k], podzial, MPI_INT, ROOT, MPI_COMM_WORLD); 
+					}
+					/* Wysłanie vektora do wszystkich */
+					MPI_Bcast( vector, size, MPI_INT, 0,  MPI_COMM_WORLD);	
+		
+					//właściwe swojej części przez wektor
+					for (i = 0; i < size; i++){
+						for (j = 0; j < size; j++){
+							res_result[i] += res_matrix[j][i] * vector[j];
+						}
+					}
+				
+					MPI_Gather(&res_result, podzial, MPI_INT, result, podzial, MPI_INT, ROOT, MPI_COMM_WORLD);	
+					execution_time = (double)(czas() - start)/(double)TIMER_SCALE; 
+					sum_execution_time += execution_time;
+				
+			
+				} 
+					if (rank == ROOT) {
+						//wyświetlanie wyniku
+						if (size <= 16) {
+							for (i = 0; i < size; i++){
+								for (j = 0; j < size; j++){
+									printf("%d\t",matrix[j][i]);
+								}
+								printf("| %d\t|", vector[i]);
+								printf("| %d\t|\n", result[i]);
+							}
+					//	printf("\nKoniec obliczeń, czas obliczeń = %f\n", execution_time);
+					}
+					printf("\nKoniec obliczeń, średni czas obliczeń = %f\n", (double)execution_time/count_loop_main);
+				}
+			} else {
+				if (rank == ROOT) {	printf("Źle dobrane parametry wielkość macierzy musi być podzielna przez liczbe procesów.\n"); }
+				}
 		} else {
-			printf("Zly paramter wywoałania programu! Musi to być liczba.\n");
+			if (rank == ROOT) { printf("Zly paramter wywoałania programu! Musi to być liczba.\n"); }
 		}
 	}	else {
 		printf("Musisz podać dwa parametry: liczba procesów, rozmiar macierzy\n");
 	}
 	
-MPI_Finalize();
+	MPI_Finalize();
 	
 return 0;
 }
