61fb11cd8848af4d617363403492768fffe0899d
Jan Mudry <jairek@o2.pl>
2012-10-20 11:17:15 +0200

    liczeni całki w sposów sekwencyjny

---
+++ liczenie_calki/zwykle.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#define TIMER_SCALE  1000000000
+long int czas() {
+	struct timespec t;
+	clock_gettime(CLOCK_MONOTONIC, &t);
+	return ((t.tv_sec * TIMER_SCALE) + t.tv_nsec);
+}
+
+double f(double x) {
+	return  pow(M_E, -x*x);
+}
+
+	// Wolfram: (e^-(x^2))dx from -2 to 2 = 1,76416
+double liczCalke(double a, double b, int n){
+	double suma =0;
+	double h = (b-a)/n;
+	int i;
+	double x = a;
+	suma -= (f(a) + f(b))/2;
+	for (i=0 ; i<= n ; i++){
+		x += h;			
+		suma += f(x);
+	}
+	return suma*h;
+}
+
+
+int main(int argc, char **argv){	
+	int i, t;
+	double a = -2;
+	double b = 2;
+	double n = 400000;
+	long int start;
+	double execution_time;
+	
+	double suma;
+	start = czas();
+	printf("Całka = %f\n", liczCalke(a,b,n));
+	printf("Koniec obliczeń, czas obliczeń = %f\n", (double)(czas() - start)/(double)TIMER_SCALE);
+	
+
+	return 0;
+}
